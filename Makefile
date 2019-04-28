e3d9b4631d702f7ce2c4cef711a1100208d791f0
Xiguo Xie <xiexiguo@live.com>
2019-04-28 22:53:20 +0800

    Add hipFFT 3d test

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
-INC = -I/usr/local/cuda/include
-LIBS = -L/usr/local/cuda/lib -lcudart -lcufft
+CUDA_INC = -I/usr/local/cuda/include
+CUDA_LIBS = -L/usr/local/cuda/lib -lcudart -lcufft
 NVCC = /usr/local/cuda/bin/nvcc
+
+HIP_INC = -I/opt/rocm/include
+HIP_LIBS = -L/opt/rocm/lib -lhip_hcc -lrocfft -lrocfft-device
+HIPCC = /opt/rocm/bin/hipcc
+hipfft3d_test: hipfft3d_test.hip.cpp
+	$(HIPCC) $(HIP_INC) $(HIP_LIBS) -o $@ $<
+
 cufft3d_test: cufft3d_test.cu
-	$(NVCC) $(INC) $(LIBS) -o $@ $<
+	$(NVCC) $(CUDA_INC) $(CUDA_LIBS) -o $@ $<
+
+clean:
+	rm hipfft3d_test cufft3d_test -f
---
+++ hipfft3d_test.hip.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <hip/hip_runtime.h>
+#include <hipfft.h>
+
+#define CHECK 1
+#define VERBOSE 1
+
+void hipda_error_check (hipError_t hipError, int line) {
+  size_t      free, total;
+  hipError_t cErr2;
+
+  cErr2 = hipGetLastError();
+  if (hipError != hipSuccess || cErr2 != hipSuccess) {
+    printf("HIP RT Error line %d\n", line);
+    printf("HIP RT1 Error: %s\n", hipGetErrorString(hipError));
+    printf("HIP RT2 Error: %s\n", hipGetErrorString(cErr2));
+    hipMemGetInfo(&free,&total);
+    printf("Free: %zu , Total: %zu\n", free, total);
+    fflush(stdout);
+    exit(-1);
+  }
+}
+
+
+void hipfft_error_check (hipfftResult_t hipfftError, int line)
+{
+  size_t      free, total;
+  hipError_t cErr2;
+
+  cErr2 = hipGetLastError();
+  if (hipfftError != HIPFFT_SUCCESS || cErr2 != hipSuccess) {
+    printf("HIP FFT Error line: %d \n", line);
+    switch (hipfftError) {
+      case HIPFFT_INVALID_PLAN:   printf("HIP FFT1 Error (HIPFFT_INVALID_PLAN)\n"); break;
+      case HIPFFT_ALLOC_FAILED:   printf("HIP FFT1 Error (HIPFFT_ALLOC_FAILED)\n"); break;
+      case HIPFFT_INVALID_VALUE:  printf("HIP FFT1 Error (HIPFFT_INVALID_VALUE)\n"); break;
+      case HIPFFT_INTERNAL_ERROR: printf("HIP FFT1 Error (HIPFFT_INTERNAL_ERROR)\n"); break;
+      case HIPFFT_EXEC_FAILED:    printf("HIP FFT1 Error (HIPFFT_EXEC_FAILED)\n"); break;
+      case HIPFFT_INVALID_SIZE:   printf("HIP FFT1 Error (HIPFFT_INVALID_SIZE)\n"); break;
+      default: printf("HIP FFT1 Error (--unimplemented--) %d %d\n", hipfftError, cErr2); break;
+    }
+    printf("HIP FFT2 Error %s \n", hipGetErrorString(cErr2));
+    hipMemGetInfo(&free,&total);
+    printf("Free: %zu , Total: %zu\n", free, total);
+    fflush(stdout);
+    exit(-1);
+  }
+}
+
+
+void ffthip_plan3d_z(hipfftHandle  &plan, const int *n) 
+{
+
+  hipfftResult_t cErr;
+
+  if (VERBOSE) printf("FFT 3D (%d-%d-%d)\n", n[0], n[1], n[2]);
+  cErr = hipfftPlan3d(&plan, n[2], n[1], n[0], HIPFFT_Z2Z);
+  if (CHECK) hipfft_error_check(cErr, __LINE__);
+  //cErr = hipfftSetStream(plan, hipda_stream);
+  //if (CHECK) hipfft_error_check(cErr, __LINE__);
+}
+
+
+
+int main()
+{
+
+  // file store data before and after fft
+  FILE *f_data_in, *f_data_out;
+  int n[3], fsign;
+  int lmem;
+  double *data_in_h, *data_out_h, *data_fft_h;
+  hipfftDoubleComplex *data_d;
+  hipfftHandle   plan;
+  hipfftResult_t cErr;
+  hipError_t  hipErr;
+
+  // read data before and after fft
+  f_data_in = fopen("data_in.dat", "r+");
+  f_data_out = fopen("data_out.dat", "r+");
+
+  // read data size
+  fscanf(f_data_in,"%d %d %d\n", &n[0], &n[1], &n[2]);
+  fscanf(f_data_in,"%d\n", &fsign);
+  if(VERBOSE) printf("FFT 3D size: %d %d %d\n", n[0], n[1], n[2]);
+  lmem = n[0] * n[1] * n[2];
+
+  data_in_h = (double*) malloc(2*lmem*sizeof(double));
+  data_out_h = (double*) malloc(2*lmem*sizeof(double));
+  data_fft_h = (double*) malloc(2*lmem*sizeof(double));
+
+  for(int i=0; i< 2*lmem; ++i)
+  {
+        fscanf(f_data_in,"%lf\n", &data_in_h[i]);
+        fscanf(f_data_out,"%lf\n", &data_out_h[i]);
+  }
+  if(VERBOSE)
+  {
+      for(int i=0; i<10; ++i) printf("%.16e %.16e\n", data_in_h[i], data_out_h[i]);
+  }
+
+  hipErr = hipMalloc(&data_d, 2*lmem*sizeof(double));
+  if(CHECK) hipda_error_check(hipErr, __LINE__);
+  hipMemcpy(data_d, data_in_h, 2*lmem*sizeof(double), hipMemcpyHostToDevice);
+
+  ffthip_plan3d_z(plan, n);
+  if ( fsign < 0  ) {
+    cErr = hipfftExecZ2Z(plan, data_d, data_d, HIPFFT_BACKWARD);
+    if (CHECK) hipfft_error_check(cErr, __LINE__);
+  }
+  else {
+    cErr = hipfftExecZ2Z(plan, data_d, data_d, HIPFFT_FORWARD);
+    if (CHECK) hipfft_error_check(cErr, __LINE__);
+  }
+
+  hipMemcpy(data_fft_h, data_d, 2*lmem*sizeof(double), hipMemcpyDeviceToHost);
+
+  for(int i=0; i<10; ++i) printf("%.16e %.16e %.16e\n", data_in_h[i], data_out_h[i],data_fft_h[i]);
+  free(data_in_h);
+  free(data_out_h);
+  free(data_fft_h);
+}
